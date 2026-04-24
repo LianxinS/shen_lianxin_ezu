5481bbd74c3a52e122e4681b3e52701840251124
lianxinS <peri15857491408@gmail.com>
2026-04-24 01:57:18 -0500

    upload

---
+++ shen_lianxin_ezu/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for shen_lianxin_ezu project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shen_lianxin_ezu.settings')
+
+application = get_asgi_application()
